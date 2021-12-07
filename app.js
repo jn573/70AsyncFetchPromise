106a4f07543b665879139a785550250e538f1434
James <cheekywena@gmail.com>
2021-12-07 10:09:09 +0200

    created async await with promise

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 async function myFunc() {
-    return 'hello'
-}
+    // return 'hello';
 
-myFunc()
-.then(res => console.log(res))
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Hallo'), 1000)
+    })
+
+        const error = false;
+
+        if (!error) {
+            const res = await promise;
+            return res;
+        } else {
+            await Promise.reject(new Error('Something went wrong'))
+        }
+
+        // const res = await promise
+        // return res
+    }
+
+    myFunc()
+        .then(res => console.log(res))
